107209696325f205a260f7405daada881b27032e
cyhuajuan <cyhuajuan@outlook.com>
2026-06-04 00:47:17 +0800

    chore: replace eslint with oxlint

--- apps/desktop/eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import config from "../../packages/config/eslint.config.mjs";
-
-export default [
-  {
-    ignores: ["src-tauri/resources/**", "src-tauri/target/**", "dist/**"],
-  },
-  ...config,
-];
